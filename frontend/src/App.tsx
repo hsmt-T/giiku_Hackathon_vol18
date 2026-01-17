a3fb690e061d2f4b04d00fa79826afe1f34e6a82
ShunKUN1182 <2250378@ecc.ac.jp>
2026-01-18 01:42:50 +0900

    画像を追加

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,28 +6,16 @@ import { Result } from "./pages/Result/Result";
 import { Sampai } from "./pages/SampaiScreen/Sampai";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Header />
-
-      {/* // 拍手だけ検知したい場合 */}
-      {/* <Camera detectMotion="clap"/> */}
-
-      {/* // 投げるだけ検知したい場合 */}
-      {/* <Camera detectMotion="throw" />  */}
-
-      {/* <Camera detectMotion="swing" />  */}
-      {/* <Camera detectMotion="bow" />  */}
-
-
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/result" element={<Result />} />
-        <Route path="/sanpai" element={<Sampai/>} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/result" element={<Result />} />
+                <Route path="/sanpai" element={<Sampai />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,30 +1,23 @@
 import { useNavigate } from "react-router-dom";
 import TpButton from "../../components/Button/Button";
 import "./Home.css";
-import jinjaImg from "../../assets/jinja.png";
+import jinjaImg from "../../assets/mainvisual.jpeg";
 
 export const Home = () => {
     const navigate = useNavigate();
     return (
-        <section
-            className="hero"
-            style={{ backgroundImage: `url(${jinjaImg})` }}
-        >
+        <section className="hero" style={{ backgroundImage: `url(${jinjaImg})` }}>
             <div className="hero__overlay" />
 
             <div className="hero__content">
                 <h1 className="hero__title">
-                    正しい参拝を学び<br />おみくじを引こう
+                    正しい参拝を学び
+                    <br />
+                    おみくじを引こう
                 </h1>
 
-                <TpButton
-                    label="参拝する"
-                    onClick={() => navigate("/sanpai")}
-                    variant="tp"
-                />
+                <TpButton label="参拝する" onClick={() => navigate("/sanpai")} variant="tp" />
             </div>
-            
         </section>
-        
     );
 };
